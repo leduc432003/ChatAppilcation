adcb94dead6d023ad1b6d753705534dd8777f604
leduc432003 <anhducle4433@gmail.com>
2024-07-21 16:25:15 +0700

    commit

---
+++ charserver/src/main/java/com/duc/charserver/model/Status.java
@@ -0,0 +1,7 @@
+package com.duc.charserver.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
---
+++ chatclient/chat-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ChatRoom from "./components/ChatRoom";
+
+function App() {
+  return <ChatRoom />;
+}
+
+export default App;
